210a0caa0d9a226d1043860ccf9300e567b4fe01
Helen Nguyen <hnguye89@uncc.edu>
2023-09-14 22:02:15 -0400

    Add packages in json

--- nodejs/main.js
+++ nodejs/main.js
@@ -4,11 +4,33 @@ fs.appendFile('my-file.txt', 'Hello Fabio!', function (err) {
   if (err) throw err;
   console.log('Saved!');
 });*/ 
-const http = require('http');
+/*  const http = require('http');
 
 http.createServer(function (req, res) {
   res.write('Hello from server!');
   res.end();
+}).listen(3000, function() {
+    console.log("Serving at http://localhost:3000");
+});*/
+
+const http = require('http');
+
+http.createServer(function (req, res) {
+    var url = req.url;
+    if(url ==='/about') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('About page');
+        res.end();
+    }
+    else if(url ==='/contact') {
+        res.writeHead(401, {'Content-Type': 'text/html'});
+        res.write('Access not authorized');
+        res.end();
+    }
+    else {
+        res.write('Hello World!');
+        res.end();
+    }
 }).listen(3000, function() {
     console.log("Serving at http://localhost:3000");
 });
