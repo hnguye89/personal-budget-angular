0d5455dbac06f74870164fe17248d98243dfdecf
Helen Nguyen <hnguye89@uncc.edu>
2023-09-17 21:10:04 -0400

    Loading data from JSON file

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const port = 4000; 
+
+app.use('/', express.static('public'));
+
+const budget = {
+  MyBudget: [
+    {
+        title: 'Eat out',
+        budget: 30
+    },
+    {
+        title: 'Rent',
+        budget: 350
+    }, 
+    {
+        title: 'Groceries',
+        budget: 90
+    },
+    {
+        title: 'Entertainment',
+        budget: 50
+    },
+    {
+        title: 'Healthcare',
+        budget: 70
+    },
+    {
+        title: 'Housing',
+        budget: 500
+    },
+    {
+        title: 'Medicine',
+        budget: 100
+    }, 
+  ]
+};
+
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!'); 
+});
+
+app.get('/budget', (req,res) => {
+    res.json(budget);
+}); 
+
+app.listen(port, () => {
+    console.log('Example app listening at http://localhost:${port}');
+});
